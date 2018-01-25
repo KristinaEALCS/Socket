484afd2cedcbb19e3290e30701ec33233a3359d2
KristinaEALCS <k.ochmirova@gmail.com>
2018-01-25 01:55:55 +0100

    yey

---
+++ Client/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clients
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient client = new TcpClient("localhost", 12000);
+            NetworkStream stream = client.GetStream();
+            StreamWriter writer = new StreamWriter(stream);
+            StreamReader reader = new StreamReader(stream);
+            string text = reader.ReadLine();
+            Console.WriteLine(text);
+            writer.WriteLine("DIR");
+            writer.WriteLine("a");
+            writer.Flush();
+            string reply = reader.ReadLine();
+            Console.ReadKey();
+            stream.Close();
+
+
+                 
+        }
+    }
+}
--- Socket/Program.cs
+++ Socket/Program.cs
@@ -1,15 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.IO;
 
-namespace Socket
+namespace Sockets
 {
     class Program
     {
         static void Main(string[] args)
         {
+            TcpListener listener = new TcpListener(IPAddress.Any,11000);
+            listener.Start();
+            Console.WriteLine("Waiting for connection");
+            Socket socket = listener.AcceptSocket();
+            Console.WriteLine("Connected");
+            NetworkStream networkStream = new NetworkStream(socket);
+            StreamReader reader = new StreamReader(networkStream);
+            StreamWriter writer = new StreamWriter(networkStream);
+            writer.AutoFlush = true;
+            writer.WriteLine("You are connected");
+
+            bool run = true;
+            while (run)
+            {
+                string command = reader.ReadLine();
+
+                switch (command.ToUpper())
+                {
+                    case "DIR":
+                        string text = reader.ReadLine();
+                        if (text == "a")
+                        {
+                            writer.WriteLine("It exists");
+                        }
+                        else
+                        {
+                            writer.WriteLine("It does not exist");
+                        }
+                        break;
+                    case "EXIT":
+                        run = false;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            networkStream.Close();
+            networkStream.Dispose();
         }
+
+        
+
+
+        
     }
 }
